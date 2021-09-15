947167b437490303615e2da66bd44a6243dc323b
Julian Bardelli <jbardelli@inlab.local>
2021-09-15 11:33:17 -0300

    added images to dataset

--- petrographic_image_train.py
+++ petrographic_image_train.py
@@ -5,13 +5,12 @@ import numpy as np
 
 from matplotlib import pyplot as plt
 from patchify import patchify
-from petrographic_image_utils import image_crop
-from petrographic_image_utils import process_img_patch
-# import segmentation_models as sm
+from petrographic_image_utils import image_crop, process_img_patch, rgb_to_2d_label
+import segmentation_models as sm
 from sklearn.preprocessing import MinMaxScaler
-# from sklearn.model_selection import train_test_split
-# from tensorflow.keras.utils import to_categorical
-# from simple_multi_unet_model import multi_unet_model, jacard_coef
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from simple_multi_unet_model import multi_unet_model, jacard_coef
 
 scaler = MinMaxScaler()
 patch_size = 256
@@ -19,17 +18,16 @@ SIZE_X = 256
 SIZE_Y = 256
 n_classes = 4  # Number of classes for segmentation
 root_directory = 'petrographic_image_dataset/'
-image_dataset_p = []
-image_dataset_c = []
+image_dataset = []
 
 for path, subdirs, files in os.walk(root_directory):
     dirname = path.split('/')[-1]
     if dirname == 'paralelos':             # Find all 'images' directories
         images = os.listdir(path)        # List of all image names in this subdirectory
         for i, image_name in enumerate(images):
-            if image_name.endswith(".tiff"):
+            if image_name.endswith(".jpg"):
                 print(image_name)
-                path_cruzados = root_directory + "cruzados/" + image_name.split('_')[0] + "_cruzados.tiff"
+                path_cruzados = root_directory + "cruzados/" + image_name.split('_')[0] + "_cruzados.jpg"
                 print(path_cruzados)
 
                 image_p = cv2.imread(path + "/" + image_name, 1)        # Read parallel nicols image as BGR
@@ -48,9 +46,8 @@ for path, subdirs, files in os.walk(root_directory):
                     for k in range(patches_p.shape[1]):
                         single_patch_p = process_img_patch(patches_p[j, k, :, :], scaler)
                         single_patch_c = process_img_patch(patches_c[j, k, :, :], scaler)
-                        # stacked = np.dstack((single_patch_p, single_patch_c))                 # Stack the two images into a 6 channel array
-                        image_dataset_p.append(single_patch_p)
-                        image_dataset_c.append(single_patch_c)
+                        stacked = np.dstack((single_patch_p, single_patch_c))
+                        image_dataset.append(stacked)
 
 # Now do the same as above for masks
 # For this specific dataset we could have added masks to the above code as masks have extension png
@@ -78,33 +75,9 @@ for path, subdirs, files in os.walk(root_directory):
                         single_patch_mask = single_patch_mask[0]  # Drop the extra unnecessary dimension that patchify adds.
                         mask_dataset.append(single_patch_mask)
 
-image_dataset_p = np.array(image_dataset_p)
-image_dataset_c = np.array(image_dataset_c)
+
+image_dataset = np.array(image_dataset)
 mask_dataset = np.array(mask_dataset)
-
-
-# image_number = random.randint(0, len(image_dataset))
-# plt.figure(figsize=(12, 6))
-# plt.subplot(121)
-# plt.imshow(np.reshape(image_dataset[image_number], (patch_size, patch_size, 3)))
-# plt.subplot(122)
-# plt.imshow(np.reshape(mask_dataset[image_number], (patch_size, patch_size, 3)))
-# plt.show()
-
-def rgb_to_2d_label(label_):
-    """
-    Supply our label masks as input in RGB format.
-    Replace pixels with specific RGB values ...
-    """
-    label_seg = np.zeros(label_.shape, dtype=np.uint8)
-    label_seg[np.all(label_ == 0, axis=-1)] = 0
-    label_seg[np.all(label_ == 1, axis=-1)] = 1
-    label_seg[np.all(label_ == 2, axis=-1)] = 2
-    label_seg[np.all(label_ == 3, axis=-1)] = 3
-    label_seg[np.all(label_ == 4, axis=-1)] = 4
-    label_seg = label_seg[:, :, 0]  # Just take the first channel, no need for all 3 channels
-    return label_seg
-
 
 labels = []
 for i in range(mask_dataset.shape[0]):
@@ -112,17 +85,15 @@ for i in range(mask_dataset.shape[0]):
     labels.append(label)
 
 labels = np.array(labels)
-
 print("Unique labels in label dataset are: ", np.unique(labels))
 
 # Another Sanity check, view few mages
-
-image_number = random.randint(0, len(image_dataset_p))
+image_number = random.randint(0, len(image_dataset))
 plt.figure(figsize=(12, 4))
 plt.subplot(131)
-plt.imshow(image_dataset_p[image_number])
+plt.imshow(image_dataset[image_number, :, :, 0:3])
 plt.subplot(132)
-plt.imshow(image_dataset_c[image_number])
+plt.imshow(image_dataset[image_number, :, :, 3:7])
 plt.subplot(133)
 plt.imshow(labels[image_number])
 plt.show()
@@ -131,7 +102,6 @@ plt.show()
 n_classes = len(np.unique(labels))
 labels_cat = to_categorical(labels, num_classes=n_classes)
 X_train, X_test, y_train, y_test = train_test_split(image_dataset, labels_cat, test_size=0.20, random_state=42)
-
 #######################################
 # Parameters for model
 # Segmentation models losses can be combined together by '+' and scaled by integer or float factor
@@ -140,7 +110,7 @@ X_train, X_test, y_train, y_test = train_test_split(image_dataset, labels_cat, t
 #                                np.ravel(labels, order='C'))
 # print(weights)
 
-weights = [0.4, 0.6]
+weights = [0.2, 0.2, 0.2, 0.2, 0.2]
 dice_loss = sm.losses.DiceLoss(class_weights=weights)
 focal_loss = sm.losses.CategoricalFocalLoss()
 total_loss = dice_loss + (1 * focal_loss)  #
--- petrographic_image_utils.py
+++ petrographic_image_utils.py
@@ -17,3 +17,19 @@ def process_img_patch(single_patch_img, scaler):
     # single_patch_img = (single_patch_img.astype('float32')) / 255.
     single_patch_img = single_patch_img[0]  # Drop the extra unnecessary dimension that patchify adds.
     return single_patch_img
+
+
+def rgb_to_2d_label(label_):
+    """
+    Supply our label masks as input in RGB format.
+    Replace pixels with specific RGB values ...
+    """
+    label_seg = np.zeros(label_.shape, dtype=np.uint8)
+    label_seg[np.all(label_ == 0, axis=-1)] = 0
+    label_seg[np.all(label_ == 1, axis=-1)] = 1
+    label_seg[np.all(label_ == 2, axis=-1)] = 2
+    label_seg[np.all(label_ == 3, axis=-1)] = 3
+    label_seg[np.all(label_ == 4, axis=-1)] = 4
+    label_seg[np.all(label_ == 5, axis=-1)] = 5
+    label_seg = label_seg[:, :, 0]  # Just take the first channel, no need for all 3 channels
+    return label_seg
